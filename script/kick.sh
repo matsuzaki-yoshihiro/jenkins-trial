f89c543af5ae1121f2a5e3ef52a8bacb83d58181
Yoshihiro Matsuzaki <y-matsuzaki@skygroup.jp>
2026-02-03 19:11:08 +0900

    Job起動スクリプト

---
+++ script/kick.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#################################################
+# 注意
+#################################################
+# JENKINS_USERNAME, JENKINS_TOKENをexport前提
+#################################################
+set -ex
+cd "$(dirname "$0")"
+
+source ./jenkins_env.sh
+
+JENKINS_FOLDER_NAME=${1:-"test-folder"}
+JOB_NAME="trial"
+
+echo "JENKINS_URL: ${JENKINS_URL}"
+
+# ジョブのビルドURL
+# フォルダ構成: [ENV_URL]/job/[FOLDER_NAME]/job/[JOB_NAME]
+JOB_URL="${JENKINS_FULL_BUILD_URL}/job/${JENKINS_FOLDER_NAME}/job/${JOB_NAME}/build"
+echo "JOB_URL: ${JOB_URL}"
+
+# Jenkins Crumb取得
+CRUMB=$(curl -s --user "${JENKINS_USERNAME}:${JENKINS_TOKEN}" "${JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
+
+# ビルド実行
+http_status=$(curl -L -s -o /dev/null -w "%{http_code}" -X POST \
+"${JOB_URL}" \
+--user "${JENKINS_USERNAME}:${JENKINS_TOKEN}" \
+-H "$CRUMB")
+
+# 201 Created: キューに追加された
+if [[ "$http_status" -eq 201 ]]; then
+  echo "Jenkins Job起動成功"
+else
+  echo "Error: Jenkins Job起動失敗 (HTTP status: $http_status)" >&2
+  exit 1
+fi
