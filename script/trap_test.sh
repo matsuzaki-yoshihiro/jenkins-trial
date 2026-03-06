e4c047774b27da271c4ea403af9b6239634148da
Yoshihiro Matsuzaki <y-matsuzaki@skygroup.jp>
2026-03-06 17:08:45 +0900

    時刻表示テスト

--- script/trap_test.sh
+++ script/trap_test.sh
@@ -13,7 +13,21 @@ echo "ls /tmp: $(ls /tmp -l)"
 echo "ls ${MK_TEMP_DIR}: $(ls ${MK_TEMP_DIR} -l)"
 
 # 現在時刻の表示（日本時間/JST, YYYY-MM-DD HH:MM:SS形式）
-echo "Current time (JST): $(TZ='Asia/Tokyo' date '+%Y-%m-%d %H:%M:%S')"
+BUILD_ALL_DESCRIPTION="
+■ 概要
+Arene : mainブランチのHEAD
+Tier1 : 集約最新タグ
+TMC : main-26cdcブランチのHEAD
+meta-luarocks : kirkstone-tmcブランチのHEAD
+の組み合わせでビルドを実行し、AreneやTMCの変更によるビルド問題を早期検出する事を目的としたジョブ
+
+■ ビルドトリガー
+90-misc/check-arene-tmc-nextジョブにより、ビルド環境の更新とbuild-allジョブの実行が行われる
+
+■ ビルド環境（現在の設定：$(TZ='Asia/Tokyo' date '+%Y-%m-%d %H:%M:%S')更新）
+"
+
+echo "${BUILD_ALL_DESCRIPTION}"
 
 echo "Sleeping for 10 seconds..."
 sleep 10
